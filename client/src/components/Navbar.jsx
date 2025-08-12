17cf12a71f2ebcff3f9bbcf50f550dbcfb48edbe
Bimsara Imash <168928858+BimsaraImash@users.noreply.github.com>
2025-08-12 23:10:04 +0530

    S2 - Creating React environment

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { assets } from '../assets'
+
+const Navbar = () => {
+  return (
+    <div>
+        <img src={assets.logo} alt="Logo" className="h-8"/>
+    </div>
+  )
+}
+
+export default Navbar
